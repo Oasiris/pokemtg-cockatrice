7feeb10057c81d7b0b53d8d6097302db83e18a82
Oasiris <teracontinens@gmail.com>
2021-12-27 06:40:57 -0500

    Mana cost converter works

--- main.ts
+++ main.ts
@@ -22,7 +22,15 @@ import cardData from './in/rawData'
 //     console.log('Hi')
 // }
 
-
+const MANA_COST_LONG_TO_SHORT = {
+    'colorless': 'C',
+    'black': 'B',
+    'white': 'W',
+    'blue': 'U',
+    'red': 'R',
+    'green': 'G',
+    'snow': 'S'
+}
 
 
 export class CardUtil {
@@ -39,19 +47,52 @@ export class CardUtil {
         const shortType = match.groups.type
         return shortType
     }
+
+    /**
+     * @param manaCostHtml A card's mana cost as an HTML string, e.g. 
+     *      '<span class="icon-wrapper"><i class="mtg mana-3"></i></span>'.
+     * @returns The card's mana cost, eg "1UW".
+     */
+    static getManaCost(manaCostHtml: string): string {
+        if (manaCostHtml === '') {
+            return ''
+        }
+        const matches: RegExpMatchArray = manaCostHtml.match(/(?<c1>i class="mtg [\w-]+")/g)
+        if (matches === null) {
+            throw new Error(`Couldn't parse manaCostHtml '${manaCostHtml}' into a mana cost`)
+        }
+        let manaCost: string = ""
+        for (let item of matches) {
+            item = item.replace(/"/g, '').replace(/i class=mtg /, '')
+            if (item.startsWith("mana")) {
+                manaCost += item[item.length - 1].toUpperCase()
+            } else if (item.startsWith("hybrid")) {
+                const left = item[item.length - 2]
+                const right = item[item.length - 1]
+                manaCost += `{${left.toUpperCase()}/${right.toUpperCase()}}`
+            } else {
+                const newLetter = MANA_COST_LONG_TO_SHORT[item]
+                if (newLetter === undefined) {
+                    throw new Error(`Can't parse item '${item}'`)
+                }
+                manaCost += newLetter
+            }
+        }
+        return manaCost
+    }
 }
 
 
 
 
 if (require.main === module) {
-    const representedTypes: Set<string> = new Set()
+    let counter = 0
     for (const data of cardData) {
-        const shortType = CardUtil.getBaseType(data.types)
-        representedTypes.add(shortType)
+        if (counter++ >= 50) {
+            console.log(data.name)
+            const manaCost = CardUtil.getManaCost(data.manaCost)
+            console.log(manaCost)
+        }
+        // TODO
     }
-    const actualTypes: string[] = Array.from(representedTypes)
-    actualTypes.sort()
-
-    console.log(actualTypes)
 }
